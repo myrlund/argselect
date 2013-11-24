3b4307833e1c74f4ecb26a26dda17eefde3eb666
Jonas Myrlund <myrlund@gmail.com>
2013-11-24 18:46:20 +0100

    Updated example.py to highlight return values.

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ from argselect import argmin, argmax
 def cube(n):
     return n ** 3
 
-minimized_arg = argmin(cube, [-1, 2, -3])
-maximized_arg = argmax(cube, [-1, 2, -3])
+argmin(cube, [-1, 2, -3])
+# => [-3]
 
-print minimized_arg
-print maximized_arg
+argmax(cube, [-1, 2, -3])
+# => [2]
